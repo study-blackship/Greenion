87938b70ef39b442d12b2d15bf5de9e8ed7053ba
bee <rkddbtjd123@gmail.com>
2021-10-22 21:11:33 +0900

    init project

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Greenion
+
 식물 커머스 플랫폼 백엔드 API
 
 ## 기획서
+
 - https://docs.google.com/presentation/d/1EPhG_hgku8JzOcRp_7DsPDC-690OQNtVoiSIO3qhS74/edit?usp=sharing
 
 ## API Document
---
+++ src/main/java/com/blackship/greenion/GreenionApplication.java
@@ -0,0 +1,13 @@
+package com.blackship.greenion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenionApplication.class, args);
+    }
+
+}
